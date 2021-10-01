02e8a4d288941cc602e7b9bf65f01694d6fdef5c
daauerbach <daniel.auerbach@dfw.wa.gov>
2021-10-01 14:37:14 -0700

    make chin stock profiles rds and make simple scrollable table

--- _make_chin_stock_profiles.R
+++ _make_chin_stock_profiles.R
@@ -17,22 +17,25 @@ stock_profiles <- purrr::map_dfr(
   ) |> 
   purrr::set_names(form_fields)
 
-stock_profiles |>  
-  dplyr::mutate(
-    dplyr::across(1:3, factor)
-  ) |> 
-  DT::datatable(
-    class = "compact row-border",
-    filter = "top",
-    rownames = FALSE,
-    options = list(
-      dom = "ltp",
-      autowidth = TRUE,
-      scrollX=TRUE,
-      pageLength = 3
-      ,columnDefs = list(
-        list(color = "pink", targets = 2:5)
-      )
-    )
-  )
+saveRDS(stock_profiles, "objects/chin_stock_profiles.rds")
 
+
+# stock_profiles |>  
+#   dplyr::mutate(
+#     dplyr::across(1:3, factor)
+#   ) |> 
+#   DT::datatable(
+#     class = "compact row-border",
+#     filter = "top",
+#     rownames = FALSE,
+#     options = list(
+#       dom = "ltp",
+#       autowidth = TRUE,
+#       scrollX=TRUE,
+#       pageLength = 3
+#       ,columnDefs = list(
+#         list(color = "pink", targets = 2:5)
+#       )
+#     )
+#   )
+# 
